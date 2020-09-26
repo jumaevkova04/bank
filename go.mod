c76cd644c42b1724ed66c9653299a97c0bf8ebf8
jumaevkova04 <jumaevkovad@gmail.com>
2020-09-26 14:16:19 +0500

    добавлена тип Status

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jumaevkova04/bank
+module github.com/jumaevkova04/bank/v2
 
 go 1.15
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 // Category  предстовляет собой категорию, в которой был совершён платёж (авто, аптеки, рестораны и т.д.).
 type Category string
 
+// Status представляет собой статус платежа.
+type Status string
+
+// Предопределённые статусы платежей.
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment предстовляет информацию о платеже.
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
